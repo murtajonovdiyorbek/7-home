3ad1668e7153cc9257f37ca29c84b138d1f82fd6
Diyorbek <muratjanovdiyorbek@gmail.com>
2026-02-22 18:59:36 +0600

    first commit

--- config/app/urls.py
+++ config/app/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import home, services_detail, services_by_category, mechanic_detail, create_mechanic, create_service, create_category
+from .views import home, services_detail, services_by_category, mechanic_detail, create_mechanic, create_service, create_category, update_service, update_category, update_mechanic, delete_service, delete_category, delete_mechanic
 
 urlpatterns = [
     path("", home, name="home"),
@@ -9,4 +9,11 @@ urlpatterns = [
     path("create/", create_mechanic, name="create_mechanic"),
     path("category/", create_category, name="create_category"),
     path("service/", create_service, name="create_service"),
+
+    path("category/<int:pk>/update/", update_category, name="update_category"),
+    path("category/<int:pk>/delete/", delete_category, name="delete_category"),
+    path("mechanic/<int:pk>/update/", update_mechanic, name="update_mechanic"),
+    path("mechanic/<int:pk>/delete/", delete_mechanic, name="delete_mechanic"),
+    path("service/<int:pk>/update/", update_service, name="update_service"),
+    path("service/<int:pk>/delete/", delete_service, name="delete_service"),
 ]
--- config/app/views.py
+++ config/app/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+from django.http import HttpRequest
 
 from .models import Service, ServiceCategory, Mechanic
 from .forms import MechanicForm, ServiceForm, ServiceCategoryForm
@@ -80,3 +81,79 @@ def create_service(request):
     else:
         form = ServiceForm()
     return render(request, "app/create_services.html", {"form": form})
+
+
+
+def update_category(request: HttpRequest, pk):
+    category = ServiceCategory.objects.get(pk=pk)
+
+    if request.method == 'POST':
+        form = ServiceCategoryForm(data=request.POST, instance=category)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+
+    form = ServiceCategoryForm(instance=category)
+    context = {"form": form, "category": category}
+    return render(request, "app/create_services.html", context)
+
+
+def delete_category(request: HttpRequest, pk):
+    category = ServiceCategory.objects.get(pk=pk)
+
+    if request.method == 'POST':
+        category.delete()
+        return redirect('home')
+
+    context = {"category": category}
+    return render(request, "app/delete.html", context)
+
+
+def update_mechanic(request: HttpRequest, pk):
+    mechanic = Mechanic.objects.get(pk=pk)
+
+    if request.method == 'POST':
+        form = MechanicForm(data=request.POST, files=request.FILES, instance=mechanic)
+        if form.is_valid():
+            form.save()
+            return redirect('mechanic_detail', mechanic_id=mechanic.id)
+
+    form = MechanicForm(instance=mechanic)
+    context = {"form": form, "mechanic": mechanic}
+    return render(request, "app/create_services.html", context)
+
+
+def delete_mechanic(request: HttpRequest, pk):
+    mechanic = Mechanic.objects.get(pk=pk)
+
+    if request.method == 'POST':
+        mechanic.delete()
+        return redirect('home')
+
+    context = {"mechanic": mechanic}
+    return render(request, "app/delete.html", context)
+
+
+def update_service(request: HttpRequest, pk):
+    service = Service.objects.get(pk=pk)
+
+    if request.method == 'POST':
+        form = ServiceForm(data=request.POST, instance=service)
+        if form.is_valid():
+            form.save()
+            return redirect('services_detail', services_id=service.id)
+
+    form = ServiceForm(instance=service)
+    context = {"form": form, "service": service}
+    return render(request, "app/create_services.html", context)
+
+
+def delete_service(request: HttpRequest, pk):
+    service = Service.objects.get(pk=pk)
+
+    if request.method == 'POST':
+        service.delete()
+        return redirect('home')
+
+    context = {"service": service}
+    return render(request, "app/delete.html", context)
